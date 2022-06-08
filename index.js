f64c0e98edbd1bb43186d3cfa671ff4e993e621e
xiaochen <953917730@qq.com>
2022-06-08 13:54:35 +0800

    js-array

--- index.js
+++ index.js
@@ -2,11 +2,19 @@
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
 //TODO
+console.log(a instanceof Array); //output false
+console.log(b instanceof Array); //output true
+
 
 
 // Write a program that multiplies each entry in the following array by 2。
 var a = [1, 2, 3, 4, 5];
 // TODO should output [2,4,6,8,10]
+for(var i=0 ; i<a.length ;i++){
+    a[i] = a[i]*2;
+}
+console.log(a);
+
 
 
 // Write the program, according to the following requirements output results.
@@ -14,13 +22,44 @@ var colors = ["Red", "Green", "White", "Black"];
 //TODO case 1 output: 'Red Green White Black'
 // case 2 output: 'Red+Green+White+Black'
 // case 3 output: 'Red,Green,White,Black'
+var a = 1;
+switch(a){
+    case 1:
+        var colors1 = colors.join(" ");
+        console.log(colors1);
+    case 2:
+        var colors2 = colors.join("+");
+        console.log(colors2);
+    case 3:
+        var colors3 = colors.toString();
+        console.log(colors3);
+        break
+}
+
+
 
 
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
-
+a.sort(function(a,b){return b - a})
+console.log(a);
 
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 //TODO should output: 'a'
+function findMost(arr){
+    var h = {};
+    var maxNum = 0;
+    var maxEle = null;
+    for(var i=0;i<arr.length;i++){
+        var a = arr[i];
+        h[a] === undefined ? h[a] = 1: (h[a]++);
+        if (h[a] > maxNum) {
+            maxEle = a;
+            maxNum = h[a];
+        }
+    }
+    return maxEle;
+}
+console.log(findMost(a));
